b7182122a2d2855a8c32c9557eeede43fa4650a5
ollie <lieshoff@gmx.de>
2019-03-12 22:25:43 +0100

    Implementation of a first Liquibase XML file access.

---
+++ src/main/java/de/ollie/archimedes/alexandrian/service/DatabaseModelSO.java
@@ -0,0 +1,15 @@
+package de.ollie.archimedes.alexandrian.service;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * A container class for database models in service environment.
+ * 
+ * @author ollie
+ *
+ */
+@Data
+@Accessors(chain = true)
+public class DatabaseModelSO {
+}
